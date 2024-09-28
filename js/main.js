e4a100ae5a12c8d5a02e170527e2f41b873d53a9
Chafic Nehme <cnehm006@uottawa.ca>
2024-09-28 14:19:43 -0400

    push thing

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAeVO91EBeTQl-Ritjsg0tZPfQ2m0Aj6wE",
+  authDomain: "hackbibi-b355d.firebaseapp.com",
+  projectId: "hackbibi-b355d",
+  storageBucket: "hackbibi-b355d.appspot.com",
+  messagingSenderId: "1078264251831",
+  appId: "1:1078264251831:web:6f5adfc2b7703a74f09432",
+  measurementId: "G-V0T9MC6NQ6"
+};
+
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
